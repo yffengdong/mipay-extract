2a450c699c660568eb94e991c829d729a16eb3b1
yunfeng <yffengdong@gmail.com>
2018-05-02 17:04:54 +0800

    fix no find tsmclient.odex
    test fix Can't find file: system/app/TSMClient/oat/arm64/TSMClient.odex/

--- extract.sh
+++ extract.sh
@@ -131,7 +131,7 @@ deodex() {
         done
         [ -z "$(ls -A $deoappdir/$app/lib/$arch)" ] && rm -rf "$deoappdir/$app/lib"
     else
-        if [[ "$app" == "UPTsmService" ]]; then
+        if [[ "$app" == "TSMClient UPTsmService" ]]; then
             echo "----> extract native library..."
             apkfile=$deoappdir/$app/$app.apk
             path=$deoappdir/$app
